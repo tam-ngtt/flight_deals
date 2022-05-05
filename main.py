33251b104d81d31ca8b0f6c449dcbbbc33407719
Tam Thi Thanh Nguyen <87509347+tam-ngtt@users.noreply.github.com>
2022-05-05 23:03:57 +0700

    Update main.py

--- main.py
+++ main.py
@@ -21,29 +21,3 @@ for city in city_data.city_name_list:
         if flight["price"] < lowest_price:
             notification.send_message(flight)
 
-
-# FLIGHT_ENDPOINT = "https://tequila-api.kiwi.com/v2/search"
-# API_KEY = "2afQ7nTqtKgIkKCEh2k_EhkIjmU8X5qr"
-# HEADERS = {
-#     "apikey": API_KEY,
-#     "Content-Encoding": "gzip"
-# }
-#
-# start_date = datetime.date.today() + datetime.timedelta(days=1)
-# end_date = datetime.date.today() + datetime.timedelta(days=6 * 30)
-# for city in city_data.city_name_list:
-#     flight_params = {
-#         "fly_from": "LON",
-#         "fly_to": city_data.city_info[city]["ct_code"],
-#         "date_from": start_date,
-#         "date_to": end_date,
-#         "nights_in_dst_from": 7,
-#         "nights_in_dst_to": 28,
-#         "flight_type": "round",
-#         "one_for_city": 1,
-#         "curr": "GBP",
-#         "max_stopovers": 0,
-#     }
-#     response = requests.get(url=FLIGHT_ENDPOINT, params=flight_params, headers=HEADERS)
-#     data = response.json()
-#     print(data)
